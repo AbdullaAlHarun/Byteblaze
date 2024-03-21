1f73050a3dff2f9b4d438b6ae89916184d9296c8
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-03-22 00:57:48 +0100

    Added Navbar and use dark and light theme functionality!

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../componants/Nav";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            
+        </div>
+    );
+};
+
+export default Home;
