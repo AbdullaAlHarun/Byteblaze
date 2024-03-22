4d5f096b90c76d61ddcb66423e2ca18e159fc08a
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-03-22 16:21:45 +0100

    Added Hero Section on Home Page!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import Nav from "../componants/Nav";
-
+import Hero from "../componants/Hero";
+import wave from "../assets/wave.svg";
 
 const Home = () => {
-    return (
-        <div>
-            <Nav></Nav>
-            
-        </div>
-    );
+  return (
+    <div className="relative min-h-[calc(100vh-116px)] flex flex-col items-center justify-center">
+        <Hero></Hero>
+        <img className="absolute bottom-0 w-full" src={wave} alt="" />
+    </div>
+  );
 };
 
 export default Home;
